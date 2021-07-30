a330d5dd8a67e332b824ac94e62df8e3b01d5ad3
Richard <rdesilvey@gmail.com>
2021-07-30 11:22:13 -0600

    Setup feature test

---
+++ spec/features/search_page_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe 'The search page' do
+  describe 'results' do
+    it 'shows a list of poems'
+  end
+end
--- spec/services/poetry_db_api/v1/get_poem_spec.rb
+++ spec/services/poetry_db_api/v1/get_poem_spec.rb
@@ -4,11 +4,10 @@ RSpec.describe PoetryDbApi::V1::ApiEndpoints do
   describe 'get_poem' do
     it 'returns expected data when successful' do
       mock_data = load_json('poem_data.json')
-      stub_request(:get, 'https://poetrydb.org/title/Ozymandias')
+      stub_request(:get, 'https://poetrydb.org/author,poemcount/Emily;10')
        .with(headers: test_headers_for_poems).to_return(status: 200, body: mock_data, headers: {})
 
-      title = 'Ozymandias'
-      response = PoetryDbApi::V1::ApiEndpoints.get_poem(title)
+      response = PoetryDbApi::V1::ApiEndpoints.get_poems('Emily', 10)
 
       expect(response[0]).to have_key(:title)
       expect(response[0]).to have_key(:author)
